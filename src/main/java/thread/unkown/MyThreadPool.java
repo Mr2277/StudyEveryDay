00758e2fe5d670c25ca800771aa1f14dc1b6bd1c
sunhao <837544713@qq.com>
2020-01-21 16:08:00 +0800

    FixThreadPool

--- src/main/java/thread/unkown/MyThreadPool.java
+++ src/main/java/thread/unkown/MyThreadPool.java
@@ -8,6 +8,7 @@ public class MyThreadPool {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         //ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor();
         //threadPoolExecutor.s
+        /*
         ExecutorService executorService=Executors.newSingleThreadExecutor();
         for(int i=0;i<=10;i++) {
               Thread thread=new Thread(new Runnable() {
@@ -18,6 +19,16 @@ public class MyThreadPool {
               });
              FutureTask<String>futureTask= (FutureTask<String>) executorService.submit(thread);
              System.out.println(futureTask.get());
+        }*/
+        ExecutorService executorService=Executors.newFixedThreadPool(4);
+        for (int i=0;i<100000000;i++){
+            System.out.println(i);
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                     System.out.println(Thread.currentThread().getName()+"######"+System.currentTimeMillis());
+                }
+            });
         }
 
     }
