12fc22312b561dac475ece12a1a67e8b1a560db9
sun <837544713@qq.com>
2020-07-18 18:54:03 +0800

    master

--- producer/src/main/java/com/example/producer/ProducerApplication.java
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProducerApplication {
 
     public static void main(String[] args) {
+        System.out.println("master");
         SpringApplication.run(ProducerApplication.class, args);
     }
 
