f3451855135c30a01a182194120e933439cf2c5d
sun <837544713@qq.com>
2020-08-01 13:15:23 +0800

    test gitignore

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,9 +1,12 @@
 package com.example.consumer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages ="com.example.consumer.dao")
+
 public class ConsumerApplication {
 
     public static void main(String[] args) {
